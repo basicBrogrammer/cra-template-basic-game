bdbe87b054890aa461175d2436e81479f3ca2155
Jeremy Ward <jeremy.ward@mckesson.com>
2021-06-28 07:37:15 -0400

    init cra template skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# cra-template-basic-game
+
+This is a [Create React App](https://github.com/facebook/create-react-app) template to get started building a basic arcade game with react.
---
+++ template/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>React Game</p>
+        <p>Score: (TBD)</p>
+        <button>Play</button>
+      </header>
+      <main className='App-main'>
+        <div>
+          <p>Hello World</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
